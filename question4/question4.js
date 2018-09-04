7c1113cf01ec11bc922f10c32d669f6ce802a7b4
taisuke11 <r.taisuke.2018@gmail.com>
2018-09-04 13:37:54 +0900

    finished question4

--- question4/question4.js
+++ question4/question4.js
@@ -1,1 +1,20 @@
 'use strict';
+let getValue = Number(prompt ("数字を入力してください"));
+let resultValue = 0;
+let calProcessString = "（";
+function calSumValue(value, sumValue = 0){
+  calProcessString += String(value);
+  sumValue += value;
+  value -= 1;
+  if(value<1){
+    resultValue = sumValue;
+    calProcessString += "の結果）";
+    return;
+  }else{
+    calProcessString += "+";
+    calSumValue(value, sumValue);
+  }
+}
+calSumValue(getValue);
+calProcessString = String(resultValue) + calProcessString;
+console.log(calProcessString);
