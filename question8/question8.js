4bd73f99a944c1010759917c827d646fb76ea2f7
taisuke11 <r.taisuke.2018@gmail.com>
2018-09-07 18:11:29 +0900

    finished question8

--- question8/question8.js
+++ question8/question8.js
@@ -1,3 +1,37 @@
 'use strict';
-// 正解用の重複無しのランダムの四桁の数字を決める
-console.log ('question8');
+// Fooをuryyyy!!に置換する
+function replaceFooText (text) {
+  const replaceRegular = /foo/g;
+  const replaceText = 'uryyyy!!';
+  text = text.replace (replaceRegular, replaceText);
+  return text;
+}
+
+// ハッシュの中の全てをFooをuryyyy!!に変換する
+function toUry (setHash) {
+  const objectNameString = '[object Object]';
+  for (const key in setHash) {
+    if (Object.prototype.toString.call (setHash[key]) === objectNameString) {
+      setHash[key] = toUry (setHash[key]);
+    } else {
+      setHash[key] = replaceFooText (setHash[key]);
+    }
+  }
+  return setHash;
+}
+
+const hash = {
+  main: {
+    first: {text: 'foobar'},
+    second: {text: 'fizzbuzz', child: {text: 'foobar'}},
+  },
+  sub: {
+    first: {text: 'fizzbuzz', child: {text: 'foobar'}},
+    second: {
+      third: {text: 'barfoo', child: {text: 'foobar'}},
+      forth: {child: {text: 'jit_foo_foo'}},
+    },
+  },
+  text: 'foofava',
+};
+console.log (toUry (hash));
