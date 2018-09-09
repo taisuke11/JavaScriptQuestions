b79be1a41e072f7c395c0d3424ffac031ab7671d
taisuke11 <r.taisuke.2018@gmail.com>
2018-09-09 10:07:07 +0900

    Fix title name of html file. Add a comment the code. Delete unnecessary parts of code.

--- question2/question2.js
+++ question2/question2.js
@@ -1,14 +1,19 @@
 'use strict';
 let getString = prompt ('文字列を入力してください。');
+// ダブルクォーテーションが入力されるものと考えてそれを削除する
 getString = getString.replace (/\"/g, '');
-let arrayOfString = getString.split (' ');
-let hashArray = {};
+let arrayOfString = '';
+arrayOfString = getString.split (' ');
+let hashArray;
+hashArray = {};
+// 単語の数を数える
 for (const arrayStr of arrayOfString) {
   let isSameStr = false;
-  if (0 === Object.keys (hashArray).length) {
+  /*
+  if (Object.keys (hashArray).length === 0) {
     hashArray[arrayStr] = 1;
     continue;
-  }
+  } */
   for (const key in hashArray) {
     if (key === arrayStr) {
       hashArray[arrayStr] = hashArray[arrayStr] + 1;
@@ -20,4 +25,7 @@ for (const arrayStr of arrayOfString) {
     hashArray[arrayStr] = 1;
   }
 }
+// 文字列に直す
+let resultStr;
+
 console.log (hashArray);
