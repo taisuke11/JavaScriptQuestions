aea1a00ab4c8d9d552066b0e09f0fa75bacaa9a1
taisuke11 <r.taisuke.2018@gmail.com>
2018-09-09 11:07:31 +0900

    Fixed to show results even with first input. Added processing when entering incorrect values. Fixed title name of html file.

--- question3/question3.js
+++ question3/question3.js
@@ -1,42 +1,54 @@
 'use strict';
 let drawFlag = false;
-let zyankenHash = {0:"グー",1:"チョキ",2:"パー"};
-let resultString = "";
+const zyankenHash = {0: 'グー', 1: 'チョキ', 2: 'パー'};
+let resultString = '';
 let getValue = 0;
 let computerValue = 0;
 let resultValue = 0;
-
-do {
-  console.log("「じゃんけん・・・」\n0.グー 1.チョキ 2.パー");
-  getValue = Number(prompt ());
-  computerValue = Math.floor(Math.random()*Math.floor(3));
-  console.log("「ぽい！」");
-  console.log("＊コンピューター：" + zyankenHash[computerValue]);
-  console.log("＊あなた：" + zyankenHash[getValue]);
-  /*
-  左がグーの時：  0 - 1 = -1  勝ち | 0 - 2 = -2 負け
-  左がチョキの時: 1 - 2 = -1　勝ち | 1 - 0 = 1 負け
-  左がパーの時:   2 - 0 = 2   勝ち | 2 - 1 = 1 負け
-  故に、左を基準として2か-1の時は勝ち、同値はあいこ、それ以外は負けになる
-  */
-  resultValue = getValue - computerValue;
-  switch(resultValue){
-    case -1:
-    case 2:
-      resultString = "「あなたの勝ち！」";
-      drawFlag = false;
-      break;
-    case 0:
-      resultString = "「アイコでしょ！」";
-      drawFlag = true;
-      break;
-    default:
-      resultString = "「あなたの負け！」";
-      drawFlag = false;
-      break;
+const intervalID = setInterval (function () {
+  console.log ('「じゃんけん・・・」');
+  // 入力する際に何を入力すればいいのかわかりやすくするために
+  // promptの方に0.グー 1.チョキ 2.パーを表示するように修正
+  getValue = prompt ('0.グー 1.チョキ 2.パー');
+  // 何も入力しない状態で変換すると0になってしまうので
+  // 空欄の場合はエラーの値を入れるようにする
+  if (getValue === '') {
+    getValue = -1;
+  } else {
+    getValue = Number (getValue);
   }
-  console.log(resultString)
-} while (drawFlag);
-
-
-
+  // ハッシュキーが存在しないデータを入力した場合
+  // 再度入力させる
+  if (typeof zyankenHash[getValue] === 'undefined') {
+    console.log ('不正な値です。0か1か2の値を再度入力してください');
+  } else {
+    computerValue = Math.floor (Math.random () * Math.floor (3));
+    console.log ('「ぽい！」');
+    console.log ('＊コンピューター：' + zyankenHash[computerValue]);
+    console.log ('＊あなた：' + zyankenHash[getValue]);
+    // 左がグーの時：  0 - 1 = -1  勝ち | 0 - 2 = -2 負け
+    // 左がチョキの時: 1 - 2 = -1　勝ち | 1 - 0 = 1 負け
+    // 左がパーの時:   2 - 0 = 2   勝ち | 2 - 1 = 1 負け
+    // 故に、左を基準として2か-1の時は勝ち、同値はあいこ、それ以外は負けになる
+    resultValue = getValue - computerValue;
+    switch (resultValue) {
+      case -1:
+      case 2:
+        resultString = '「あなたの勝ち！」';
+        drawFlag = false;
+        break;
+      case 0:
+        resultString = '「アイコでしょ！」';
+        drawFlag = true;
+        break;
+      default:
+        resultString = '「あなたの負け！」';
+        drawFlag = false;
+        break;
+    }
+    console.log (resultString);
+    if (drawFlag === false) {
+      clearInterval (intervalID);
+    }
+  }
+}, 100);
