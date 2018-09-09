e658a3cb9c145be77573c124426bf9a8f5693ccf
taisuke11 <r.taisuke.2018@gmail.com>
2018-09-09 16:05:49 +0900

    Fixed title name of Html file of Question 7. Added error handling when entering Question 7.

--- question7/question7.js
+++ question7/question7.js
@@ -24,37 +24,51 @@ const ansewerNumberArray = getAnsewerNumber (
 let playCount = 1;
 const intervalID = setInterval (function () {
   let getNumberArray = [];
-  getNumberArray = prompt ('四桁の数字は？').split ('');
-  const array0_4 = [0, 1, 2, 3];
-  // このままだと文字列なので数字に変換する
-  array0_4.forEach (function (value) {
-    getNumberArray[value] = Number (getNumberArray[value]);
-  });
-  // hitとblowの数を求める
-  let hitCount = 0;
-  let blowCount = 0;
-  for (const count1 of [0, 1, 2, 3]) {
-    for (const count2 of [0, 1, 2, 3]) {
-      if (ansewerNumberArray[count1] === getNumberArray[count2]) {
-        if (count1 === count2) {
-          hitCount += 1;
-          break;
-        } else {
-          blowCount += 1;
-          break;
+  const tempValue = prompt ('四桁の数字は？');
+  // 空欄、文字列、小数点を含む数字、4桁でない数字はエラーとして処理しますが
+  // 同じ数字を二つ以上入れるのは戦略としてありだと思ったので
+  // エラー処理は入れていません
+  if (
+    isNaN (Number (tempValue)) ||
+    tempValue.length !== 4 ||
+    tempValue.indexOf ('.') !== -1 ||
+    tempValue.indexOf (' ') !== -1 ||
+    tempValue.indexOf ('　') !== -1
+  ) {
+    console.log ('不正な値です。0～9から四桁の数字を入力してください');
+  } else {
+    getNumberArray = tempValue.split ('');
+    const array0_4 = [0, 1, 2, 3];
+    // このままだと文字列なので数字に変換する
+    array0_4.forEach (function (value) {
+      getNumberArray[value] = Number (getNumberArray[value]);
+    });
+    // hitとblowの数を求める
+    let hitCount = 0;
+    let blowCount = 0;
+    for (const count1 of [0, 1, 2, 3]) {
+      for (const count2 of [0, 1, 2, 3]) {
+        if (ansewerNumberArray[count1] === getNumberArray[count2]) {
+          if (count1 === count2) {
+            hitCount += 1;
+            break;
+          } else {
+            blowCount += 1;
+            break;
+          }
         }
       }
     }
-  }
-  // 結果
-  console.log ('入力した数字:' + getNumberArray.join (''));
-  if (hitCount === 4) {
-    console.log (String (playCount) + '回で正解！');
-    clearInterval (intervalID);
-  } else {
-    console.log (
-      '外れ：' + String (hitCount) + 'Hits,' + String (blowCount) + 'Blow'
-    );
-    playCount += 1;
+    // 結果。
+    console.log ('入力した数字:' + getNumberArray.join (''));
+    if (hitCount === 4) {
+      console.log (String (playCount) + '回で正解！');
+      clearInterval (intervalID);
+    } else {
+      console.log (
+        '外れ：' + String (hitCount) + 'Hits,' + String (blowCount) + 'Blow'
+      );
+      playCount += 1;
+    }
   }
 }, 100);
