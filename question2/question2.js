42229879b28e0f82e258fc441ca5b3d9871994af
taisuke11 <r.taisuke.2018@gmail.com>
2018-10-01 11:18:41 +0900

    Fixed content pointed out by pull request for Question2

--- question2/question2.js
+++ question2/question2.js
@@ -1,34 +1,33 @@
 'use strict';
-let getString = prompt ('文字列を入力してください。');
+let word = prompt ('文字列を入力してください。');
 // ダブルクォーテーションが入力されるものと考えてそれを削除する
-getString = getString.replace (/\"/g, '');
-let arrayOfString = '';
-arrayOfString = getString.split (' ');
-let hashArray;
-hashArray = {};
+word = word.replace (/\"/g, '');
+const arrayOfWords = word.split (' ');
+let hashOfWordCount;
+hashOfWordCount = {};
 // 単語の数を数える
-for (const arrayStr of arrayOfString) {
-  let isSameStr = false;
-  for (const key in hashArray) {
-    if (key === arrayStr) {
-      hashArray[arrayStr] = hashArray[arrayStr] + 1;
-      isSameStr = true;
+for (const value of arrayOfWords) {
+  let isSameWord = false;
+  for (const key in hashOfWordCount) {
+    if (key === value) {
+      hashOfWordCount[value] = hashOfWordCount[value] + 1;
+      isSameWord = true;
       break;
     }
   }
-  if (isSameStr === false) {
-    hashArray[arrayStr] = 1;
+  if (isSameWord === false) {
+    hashOfWordCount[value] = 1;
   }
 }
 // 初めてファイルを開いたときと
 // ファイルを開いた状態で更新をかけた場合での表示が異なるので
 // 統一するために文字列に変換する
 let resultStr = '{';
-for (const key in hashArray) {
+for (const key in hashOfWordCount) {
   if (resultStr !== '{') {
     resultStr += ',';
   }
-  resultStr += key + ':' + String (hashArray[key]);
+  resultStr += key + ':' + String (hashOfWordCount[key]);
 }
 resultStr += '}';
 
