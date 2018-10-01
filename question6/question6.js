f73dc987e8f4ff72118f5b6d17a31f775c4ae0d4
taisuke11 <r.taisuke.2018@gmail.com>
2018-10-01 16:44:11 +0900

    Fixed content pointed out by pull request for Question6

--- question6/question6.js
+++ question6/question6.js
@@ -1,29 +1,20 @@
 'use strict';
-const answerNumber = Math.floor (Math.random () * Math.floor (101));
+const answerNumber = Math.floor (Math.random () * 101);
 let correctFlag = false;
 let count = 0;
+//未入力も含めて整数でない値は、-1を返す
+function isIntNumber (value) {
+  value = String (value);
+  if (/^(\-|\+)?([0-9]+)$/.test (value)) return Number (value);
+  return -1;
+}
 console.log ('0-100で数字を当てて');
 const intervalID = setInterval (function () {
-  let hintMessage = '';
   let getNumber = prompt ('数字を入力してください');
-  // 何も入力しない状態や半角スペースや全角スペースで変換すると0になってしまうので
-  // 空欄の場合はエラーの値を入れるようにする
-  if (
-    getNumber === '' ||
-    getNumber.indexOf (' ') !== -1 ||
-    getNumber.indexOf ('　') !== -1
-  ) {
-    getNumber = -1;
-  } else {
-    getNumber = Number (getNumber);
-  }
+  getNumber = isIntNumber (getNumber);
   // 範囲外の数字もしくは整数以外が入力されたら
   // 再度入力させる
-  if (
-    getNumber < 0 ||
-    getNumber > 100 ||
-    Math.round (getNumber) !== getNumber
-  ) {
+  if (getNumber < 0 || getNumber > 100) {
     console.log ('不正な値です。0-100での数字を再度入力してください');
   } else {
     // 正しい値が入力された時だけカウントする
@@ -32,17 +23,12 @@ const intervalID = setInterval (function () {
     // 入力した数字は見えるようにしています
     console.log ('入力した数字：' + String (getNumber));
     if (getNumber > answerNumber) {
-      hintMessage = 'もっと下';
+      console.log ('もっと下');
     } else if (getNumber < answerNumber) {
-      hintMessage = 'もっと上';
+      console.log ('もっと上');
     } else {
-      correctFlag = true;
       console.log ('正解！・・・' + String (count) + '回目で当てました');
       clearInterval (intervalID);
-    }
-    // 正解でない場合は、ヒントを表示する
-    if (correctFlag === false) {
-      console.log (hintMessage);
     }
   }
 }, 100);
